4edaeca40e410c19ed697fc18c28a3515a97f09a
Mahdi Yari <m.yary15@gmail.com>
2020-04-17 18:37:18 +0430

    Standard JS

--- src/queue.js
+++ src/queue.js
@@ -1,105 +1,103 @@
-function createQueue(intialCapacity) {
-	var that = {};
-	var head = 0;
-	var tail = 0;
-	var length = 0;
-	var initialCapacity = intialCapacity;
-	var currentSize = (typeof initialCapacity === undefined) ? initialCapacity : 200;
-	var container = [];
-	container.length=currentSize;
+function createQueue (intialCapacity) {
+  var that = {}
+  var head = 0
+  var tail = 0
+  var length = 0
+  var initialCapacity = intialCapacity
+  var currentSize = initialCapacity || 200
+  var container = []
+  container.length = currentSize
 
-	function doubling() {
-		var currentSource = head;
-		var currentTarget = 0;
-		var newContainer = [];
-		newContainer.length = 2*currentSize;
+  function doubling () {
+    var currentSource = head
+    var currentTarget = 0
+    var newContainer = []
+    newContainer.length = 2 * currentSize
 
-		while (currentTarget < currentSize) {
-			newContainer[currentTarget] = container[currentSource];
-			currentSource++;
-			currentTarget++;
-			if (currentSource == currentSize) {
-				currentSource = 0;
-			}
-		}
-		container = newContainer;
-		head = 0;
-		tail = currentSize;
-		currentSize *= 2;
-	}
+    while (currentTarget < currentSize) {
+      newContainer[currentTarget] = container[currentSource]
+      currentSource++
+      currentTarget++
+      if (currentSource === currentSize) {
+        currentSource = 0
+      }
+    }
+    container = newContainer
+    head = 0
+    tail = currentSize
+    currentSize *= 2
+  }
 
-	function shrink() {
-		var currentSource = head;
-		var currentTarget = 0;
-		var newContainer = [];
-		newContainer.length = currentSize/4;
+  function shrink () {
+    var currentSource = head
+    var currentTarget = 0
+    var newContainer = []
+    newContainer.length = currentSize / 4
 
-		while (currentTarget < currentSize) {
-			newContainer[currentTarget] = container[currentSource];
-			currentSource++;
-			currentTarget++;
-			if (currentSource == currentSize) {
-				currentSource = 0;
-			}
-		}
-		container = newContainer;
-		head = 0;
-		tail = currentSize;
-		currentSize /= 4;
-	}
+    while (currentTarget < currentSize) {
+      newContainer[currentTarget] = container[currentSource]
+      currentSource++
+      currentTarget++
+      if (currentSource === currentSize) {
+        currentSource = 0
+      }
+    }
+    container = newContainer
+    head = 0
+    tail = currentSize
+    currentSize /= 4
+  }
 
-	that.push = function(element) {
-		if (length == currentSize) {
-			doubling();
-		}
-		container[tail] = element;
-		length++;
-		tail++;
-		if (tail == currentSize) {
-			tail = 0;
-		}
-	};
+  that.push = function (element) {
+    if (length === currentSize) {
+      doubling()
+    }
+    container[tail] = element
+    length++
+    tail++
+    if (tail === currentSize) {
+      tail = 0
+    }
+  }
 
-	that.shift = function() {
-		if (length === 0) {
-			return null;
-		}
-		tmp = container[head];
-		head++;
-		length--;
-		if (head == currentSize) {
-			head = 0;
-		}
-		if (length == currentSize/4 && length > initialCapacity) {
-			shrink();
-		}
-		return tmp;
-	};
+  that.shift = function () {
+    if (length === 0) {
+      return null
+    }
+    const tmp = container[head]
+    head++
+    length--
+    if (head === currentSize) {
+      head = 0
+    }
+    if (length === currentSize / 4 && length > initialCapacity) {
+      shrink()
+    }
+    return tmp
+  }
 
+  that.front = function () {
+    if (length === 0) {
+      return null
+    }
+    return container[head]
+  }
 
-	that.front = function() {
-		if (length === 0) {
-			return null;
-		}
-		return container[head];
-	};
+  that.length = function () {
+    return length
+  }
 
-	that.length = function() {
-		return length;
-	};
+  that.isEmpty = function () {
+    return length === 0
+  }
 
-	that.isEmpty = function() {
-		return length === 0;
-	};
+  that.indexOf = function (element) {
+    return container.indexOf(element)
+  }
 
-	that.indexOf = function(element) {
-		return container.indexOf(element)
-	};
-
-	return that;
+  return that
 }
 
 module.exports = {
-	createQueue : createQueue
-};
-
+  createQueue: createQueue
+}
